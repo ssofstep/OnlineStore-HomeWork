55a7d847d21c03dbfd3571940c8f335122abee93
Sofia <ssofstep7627@gmail.com>
2024-09-20 14:26:56 +0300

    done

--- tests/test_Category.py
+++ tests/test_Category.py
@@ -20,6 +20,25 @@ def class_category() -> Category:
 def test_init(class_category: Category) -> None:
     assert class_category.name == "Смартфоны"
     assert class_category.description == "Смартфоны, как средство коммуникации и получения дополнительных функций"
-    assert class_category.products[0].name == "Samsung Galaxy S23 Ultra"
+    assert class_category.products == (
+        "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.Iphone 15, 210000.0 "
+        "руб. Остаток: 8 шт.Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
+    )
     assert class_category.category_count == 1
     assert class_category.product_count == 3
+
+
+def test_add_product(class_category: Category) -> None:
+    class_category.add_product(Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7))
+    assert class_category.products == (
+        "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.Iphone 15, 210000.0 "
+        'руб. Остаток: 8 шт.Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт.55" '
+        "QLED 4K, 123000.0 руб. Остаток: 7 шт."
+    )
+
+
+def test_products(class_category: Category) -> None:
+    assert class_category.products == (
+        "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт.Iphone 15, 210000.0 "
+        "руб. Остаток: 8 шт.Xiaomi Redmi Note 11, 31000.0 руб. Остаток: 14 шт."
+    )
