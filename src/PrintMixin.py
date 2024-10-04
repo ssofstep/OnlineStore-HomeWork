5bc4d5b3b2d08356bedc71115a8966637356376c
Sofia <ssofstep7627@gmail.com>
2024-10-04 13:30:09 +0300

    done homework

--- src/PrintMixin.py
+++ src/PrintMixin.py
@@ -2,8 +2,13 @@ class PrintMixin:
     """Класс-миксин, который будет печатать в консоль информацию о том, от какого класса и с
     какими параметрами был создан объект"""
 
-    def __init__(self):
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self) -> None:
         print(repr(self))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})"
